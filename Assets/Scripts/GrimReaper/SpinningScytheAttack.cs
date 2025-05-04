5110bc894bcf40330ace6d47c08a98299ca91303
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2025-05-04 18:51:18 +0300

    added spinning scythe

--- Assets/Scripts/GrimReaper/SpinningScytheAttack.cs
+++ Assets/Scripts/GrimReaper/SpinningScytheAttack.cs
@@ -1,16 +1,49 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpinningScytheAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int wallsHitsBeforeDestroy = 3;
+    [SerializeField] private float speed = 3f;
+    private Rigidbody2D _rb;
+    
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartAttack()
     {
-        
+        // Vector2 direction = Random.insideUnitCircle.normalized;
+        StartCoroutine(StartMovement());
+    }
+
+    private IEnumerator StartMovement()
+    {
+        yield return new WaitForEndOfFrame();
+        Vector2 direction = Random.insideUnitCircle.normalized;
+        _rb.linearVelocity = direction * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            wallsHitsBeforeDestroy--;
+            if (wallsHitsBeforeDestroy <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (other.CompareTag("Creatures"))
+        {
+            // Handle damage to the creature
+            var creature = other.GetComponent<CreatureCore>();
+            if (creature != null)
+            {
+                creature.Death();
+            }
+            // Destroy(gameObject);
+        }
     }
 }
